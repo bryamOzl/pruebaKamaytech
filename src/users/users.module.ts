7c7751c2cdf298c98bc39729269dc226670a30bb
bryamozl <bryan95_19@hotmail.com>
2023-04-22 13:14:07 -0500

    create user services

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/users.schemas';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './interfaces/users.interfaces';
+import { CreateUserDTO } from './dto/users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
+
+    async getUsers(): Promise<User[]> {
+        const users = await this.userModel.find()
+        return users;
+    }
+
+    async getUser(userID: string): Promise<User> {
+        const user = await this.userModel.findById(userID);
+        return user;
+    }
+
+    async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+        const user = new this.userModel(createUserDTO);
+        await user.save();
+        return user;
+    }
+
+    async deleteUser(userID: string): Promise<User> {
+        const deletedUser = await this.userModel.findByIdAndDelete(userID);
+        return deletedUser;
+    }
+
+    async updateUser(userID: string, createUserDTO: CreateUserDTO): Promise<User> {
+        const updatedUser = await this.userModel.findByIdAndUpdate(userID, createUserDTO, { new: true });
+        return updatedUser;
+    }
+
+}
