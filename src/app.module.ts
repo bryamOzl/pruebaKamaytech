bd61022774a3e552fcfc0b998b53b90a68f6a4a7
bryamozl <bryan95_19@hotmail.com>
2023-04-22 13:01:24 -0500

    create userDTO and connection database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/users',{useNewUrlParser: true}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
